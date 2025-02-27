6293ca5e9277c15f92d9fbe4d43496b7084963a3
ProtoSg <diegoalbertosalazar177@gmail.com>
2025-02-27 17:09:10 -0500

    feat: update transactionForm container

--- src/modules/transactionForm/TransactionForm.tsx
+++ src/modules/transactionForm/TransactionForm.tsx
@@ -1,23 +1,27 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { SubmitHandler, useForm } from "react-hook-form"
 import { Box } from "../core/components"
-import { InputForm, ModalConfirmation, SelectForm } from "./components"
+import { InputForm, ModalAddCategory, ModalConfirmation, SelectForm } from "./components"
 import { FormValues, schema } from "./models/form.model"
 import { ItemModel } from "./models/item.model"
 import { useItem } from "./hooks/useItem"
 import { toast } from "sonner"
+import { Option } from "./models/option.model"
+import { useCategories } from "../../hooks/useCategories"
 
 export const TransactionForm = () => {
   const { control, handleSubmit, formState: { errors }, reset, trigger } = useForm<FormValues>({
     resolver: zodResolver(schema),
     defaultValues: {
       type: "gasto",
+      category: "",
       product: "",
       money: 0
     }
   })
 
   const { addItem, setIncome, setExpense } = useItem()
+  const { categories } = useCategories()
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     const item = new ItemModel(data)
@@ -40,13 +44,30 @@ export const TransactionForm = () => {
     modal.showModal()
   }
 
+  const optionsType: Option[] = [
+    { name: "Gasto", value: "gasto" },
+    { name: "Ingreso", value: "ingreso" }
+  ]
+
   return (
     <Box>
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col gap-4"
       >
-        <SelectForm control={control} />
+        <fieldset className="flex gap-4">
+          <SelectForm
+            control={control}
+            label="Tipo"
+            options={optionsType}
+          />
+          <SelectForm
+            control={control}
+            label="Categoría"
+            options={categories}
+            add
+          />
+        </fieldset>
         <fieldset className="flex gap-4">
           <InputForm name="product" label="Producto" placeholder="Galleta" control={control} type="text" error={errors.product} />
           <InputForm name="money" label="Dinero" placeholder="S/ 600.00" control={control} type="number" error={errors.money} />
@@ -60,6 +81,7 @@ export const TransactionForm = () => {
         </button>
         <ModalConfirmation />
       </form>
+      <ModalAddCategory />
     </Box>
   )
 }
