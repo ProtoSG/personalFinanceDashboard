3ece0f1ae66459a48ee912e45677de3f4f4242b9
ProtoSg <diegoalbertosalazar177@gmail.com>
2025-02-27 17:08:43 -0500

    feat: add and update form and option models

--- src/modules/transactionForm/models/form.model.ts
+++ src/modules/transactionForm/models/form.model.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const schema = z.object({
   type: z.string(),
+  category: z.string(),
   product: z.string().min(1, "El campo no puede estar vacío"),
   money: z.preprocess(
     (val) => (typeof val === "string" ? parseFloat(val) : val),
